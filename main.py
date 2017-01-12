525ceabc8c65cdf7031648ca0f2ad39f8983eb31
unknown <>
2017-01-12 09:52:10 -0500

    test

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import random
+import dic.py
+
+play = True
+guess = ""
+
+hangman_pic = ("""
+
+""")
+
+
+while play == True
+    guess = input("Guess a Letter:")
+    if guess in word:
+        print("you are correct!")
+    
+    else:
+        print("you are wrong!")
+
 
