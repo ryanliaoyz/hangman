92c4e53c4f7d91e10285b94138e6610865cc66a3
unknown <>
2017-01-18 14:47:59 -0500

    final draft probably

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-#import random
-#import dic.py
+import random
+
 
 play = True
 GuessAttempts = 0
 RightWrong = 0
+GuessStatus = []
+CorrectAnswer = []
 #UsedGuess = []
-word = "procrastinating"
-GuessStatus = ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'] #GuessStatus[0-14]
-CorrectAnswer = ['p','r','o','c','r','a','s','t','i','n','a','t','i','n','g']
+
+fp=open("WordBank.txt")
+WordPool = fp.readlines()
+#print(WordPool)
+
+word = WordPool[random.randint(0,100)]
+print(word)
+#word.replace("\n","")
+Wordlength = len(word) - 1
+for Wordlength in range(0,Wordlength):
+#$    print(Wordlength)
+#$for letter in word:
+    GuessStatus.append('_')
+    CorrectAnswer.append(word[Wordlength])
+print(GuessStatus)
+print(CorrectAnswer)
+
+#$word = "procrastinating"
+#$GuessStatus = ['_','_','_','_','_','_','_','_','_','_','_','_','_','_','_'] #GuessStatus[0-14]
+#$CorrectAnswer = ['p','r','o','c','r','a','s','t','i','n','a','t','i','n','g']
 
 HangmanPic = ('''
 --------
@@ -68,7 +87,8 @@ HangmanPic = ('''
 while GuessAttempts <= 6:
     if GuessStatus != CorrectAnswer:
         guess = input("Guess a Letter:")
-        for i in range(0,15):
+        #$for i in range(0,15):
+        for i in range(0,Wordlength):
             if word[i] == guess:
                 GuessStatus[i] = word[i]
                 RightWrong = 1
